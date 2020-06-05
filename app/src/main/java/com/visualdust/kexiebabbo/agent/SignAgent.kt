48cc0c1d455f58e3b300712e756855d87c433482
VisualDust <VisualDust@outlook.com>
2020-06-05 22:13:57 +0800

    project constructing

--- app/src/main/java/com/visualdust/kexiebabbo/agent/SignAgent.kt
+++ app/src/main/java/com/visualdust/kexiebabbo/agent/SignAgent.kt
@@ -14,10 +14,10 @@ class SignMachine {
         var location: String,
         var id: Long,
         var name: String,
-        var time: Long = 0
+        var time: Double = 0.0
     ) {
         override fun toString(): String {
-            return "[dept]$dept,[location]$location,[userid]$id,[username]$name"
+            return "[dept]$dept,[location]$location,[userid]$id,[username]$name,[time]$time"
         }
     }
 
@@ -40,15 +40,13 @@ class SignMachine {
 
     fun signOut(id: String): Boolean = postSignOut(id).body!!.string().contains("成功")
 
-    fun getAttendances() = get(R.serviceAddress + R.div + R.attendancesListAPIName)
+    private fun getAttendancesResponse() = get(R.serviceAddress + R.div + R.attendancesListAPIName)
 
-    fun getRankTopFive() = get(R.serviceAddress + R.div + R.rankTopFiveAttendanceAPIName)
+    private fun getTopFiveAttendancesResponse() = get(R.serviceAddress + R.div + R.rankTopFiveAttendanceAPIName)
 
     fun getAttendanceList(): ArrayList<Attendance> {
-        var status = false
-        var response = getAttendances().body!!.string()
-        println(response)
-        var attendanceList = ArrayList<Attendance>()
+        val response = getAttendancesResponse().body!!.string()
+        val attendanceList = ArrayList<Attendance>()
         var pos_start = 0
         var pos_end = 0
         while (true) {
@@ -68,15 +66,66 @@ class SignMachine {
             pos_start = response.indexOf("username\":\"", pos_end) + 11
             pos_end = response.indexOf("\"}", pos_start)
             val username = response.substring(pos_start, pos_end)
+
             attendanceList.add(Attendance(dept, location, userid.toLong(), username))
         }
         return attendanceList
     }
+
+    fun getTopFiveAttendanceList(): ArrayList<Attendance> {
+        val response = getTopFiveAttendancesResponse().body!!.string()
+        val topFiveAttendanceList = ArrayList<Attendance>()
+        var pos_start = 0
+        var pos_end = 0
+        while (true) {
+            pos_start = response.indexOf("userid\":", pos_end) + 9
+            if (pos_start <= pos_end || pos_start == -1) break
+            pos_end = response.indexOf(",\"", pos_start)
+            val userid = response.substring(pos_start, pos_end)
+
+            pos_start = response.indexOf("username\":\"", pos_end) + 11
+            pos_end = response.indexOf("\",", pos_start)
+            val username = response.substring(pos_start, pos_end)
+
+            pos_start = response.indexOf("dept\":\"", pos_end) + 7
+            pos_end = response.indexOf("\",\"", pos_start)
+            val dept = response.substring(pos_start, pos_end)
+
+            pos_start = response.indexOf("location\":\"", pos_end) + 11
+            pos_end = response.indexOf("\",\"", pos_start)
+            val location = response.substring(pos_start, pos_end)
+
+            pos_start = response.indexOf("allTime\":", pos_end) + 9
+            pos_end = response.indexOf(",\"", pos_start)
+            val allTime = response.substring(pos_start, pos_end).toDouble() * 60
+
+            topFiveAttendanceList.add(
+                Attendance(
+                    dept,
+                    location,
+                    userid.toLong(),
+                    username,
+                    allTime
+                )
+            )
+        }
+        return topFiveAttendanceList
+    }
+
+    private constructor()
+
+    companion object {
+        private val signMachine = SignMachine()
+
+        @JvmStatic
+        fun getMachine() = signMachine
+    }
 }
 
 fun main() {
-    var agent = SignMachine()
+    val agent = SignMachine.getMachine()
 //    println(agent.signOut("1900420217"))
 //    println(agent.signIn("1900420217"))
-    println(agent.getAttendanceList()[0])
+//    println(agent.getAttendanceList()[0])
+//    println(agent.getTopFiveAttendanceList()[1])
 }
